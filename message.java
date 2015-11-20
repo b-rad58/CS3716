54f1b12a40c245279e5ed9f6a1def6734377accb
b-rad58 <bmg610@mun.ca>
2015-11-20 09:25:39 -0330

    fist commit

---
+++ message.java
@@ -0,0 +1,10 @@
+//import java.util.*;
+//public class message{
+	//public void all(Object o,society s)
+	//{send(o,s);}
+	//public void one(Object o,student s)
+	//{send (o,s);}
+//}
+
+
+//Removed to allow for compile. will be implemented with a notification system later in development.
---
+++ student.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class student {
+private String stuName;
+private String stuNum;
+private String DOB;
+private String Pos;
+    public String getName()
+    {return stuName;}
+	
+    public void setName(String b)
+    {stuName=b;}
+	
+    public String getNum()
+    {return stuNum;}
+	
+    public void setStuNum(String b)
+    {stuNum=b;}
+	
+    public String getDOB()
+    {return DOB;}
+	
+    public void setDOB(String b)
+    {DOB=b;}
+
+    public String getPos()
+    {return DOB;}
+	
+    public void setPos(String b)
+    {DOB=b;}
+}
