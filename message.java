e0c36026202835da7109d742124dee8cc9f9c3bc
rjm217 <rjm217@mun.ca>
2015-11-23 19:35:48 -0330

    Update message.java

--- message.java
+++ message.java
@@ -1,10 +1,12 @@
-//import java.util.*;
-//public class message{
-	//public void all(Object o,society s)
-	//{send(o,s);}
-	//public void one(Object o,student s)
-	//{send (o,s);}
-//}
+import java.util.*;
+public class message{
+	public void all(Object o,society s)
+	{for (int i=0; i <s.memberSize(); i++)
+              {student b = s.getAt(i);
+               one(o,b);
+              }
+        }
+	public void one(Object o,student s)
+	{s.getMailbox().addMessage(o);}
+}
 
-
-//Removed to allow for compile. will be implemented with a notification system later in development.
