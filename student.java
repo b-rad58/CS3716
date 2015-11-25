76bcf794cd87b7aea60908e9ceafe330e43510c7
rjm217 <rjm217@mun.ca>
2015-11-24 22:30:32 -0330

    Update student.java

--- student.java
+++ student.java
@@ -12,10 +12,10 @@ private mailbox owned;
     {return stuName;}
     
     public String toString()
-   {String b="the student's name is : "+this.getName()+"n\";
-    b="the student's number is : "+this.getNum()+"n\";
-    b="the student's date of birth is : "+this.getDOB()+"n\";
-    b="the student's position is : "+this.getPos()+"n\";
+   {String b="the student's name is : "+this.getName()+"n\ ";
+    b="the student's number is : "+this.getNum()+"n\ ";
+    b="the student's date of birth is : "+this.getDOB()+"n\ ";
+    b="the student's position is : "+this.getPos()+"n\ "  ;
    }
 	
     public void setName(String b)
